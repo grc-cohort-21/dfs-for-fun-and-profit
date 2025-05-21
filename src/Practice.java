907044d284f9c9141cc20e0c4aaad3a02edd4415
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-05-20 23:49:14 -0700

    Completed allOdd and passed tests

--- src/Practice.java
+++ src/Practice.java
@@ -116,7 +116,18 @@ public class Practice {
    * @return true if all reachable vertices hold odd values, false otherwise
    */
   public boolean allOdd(Vertex<Integer> vertex) {
-    return true;
+    Set<Vertex<Integer>> visited = new HashSet<>();
+    return allOdd(vertex, visited);
+  }
+  public boolean allOdd(Vertex<Integer> vertex, Set<Vertex<Integer>> visited) {
+    if(vertex == null || visited.contains(vertex)) return true;
+    visited.add(vertex);
+    boolean collective = true;
+    if(vertex.data % 2 == 0) return collective = collective && false;
+    for(Vertex<Integer> neighbor : vertex.neighbors) {
+      collective = collective && allOdd(neighbor, visited);
+    }
+    return collective;
   }
 
   /**
